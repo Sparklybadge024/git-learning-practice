eb91d91c3ffbe4a6b5550a0d09879f536d19c80f
Sparklybadge024 <sundlirishabh55@gmail.com>
2026-03-11 10:37:10 +0530

    Update Java message and HTML content

--- main.java
+++ main.java
@@ -1,6 +1,6 @@
 public class main {
     public static void main(String[] args) {
-        System.out.println("Git Java practice");
+        System.out.println("Git Java practice started");
         System.out.println("Learning Version Control");
     }
 }
